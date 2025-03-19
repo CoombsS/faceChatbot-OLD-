e80fde455ed6a40776d4e7ad4011713190698aaf
CoombsS <CoombsS@jbu.edu>
2025-03-19 17:50:40 -0500

    no openai for you

--- deepface_env/diagrams/keys.py
+++ deepface_env/diagrams/keys.py
@@ -1,7 +1,7 @@
 import openai
 
 from openai import OpenAI
-
+#This key has been revoked. No free OpenAI for you!
 client = OpenAI(
   api_key="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"
 )
